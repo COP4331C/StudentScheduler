0de58a67722d41495e3b2adec60687acc96f16ec
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-12-09 18:43:13 -0500

    Add files via upload

---
+++ api/getTasks.php
@@ -0,0 +1,26 @@
+<?php
+	include("config.php");
+	include("session.php");
+	session_start();
+     $inData = json_decode(file_get_contents('php://input'), true);
+	$sql = "select * from tasks where userid = '{$_SESSION['user_id']}' ";
+	$result = mysqli_query($db,$sql);
+	$json_array = array();
+	if($result->num_rows > 0)
+	{
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$json_array[] = $row;
+		}
+          $message = json_encode($json_array);
+     	header('Content-type: application/json');
+     	echo $message;
+	}
+     else
+     {
+               $arr['none'] = 'There are no task today yippie';
+               $message = json_encode($arr);
+          	header('Content-type: application/json');
+          	echo $message;
+     }
+?>
