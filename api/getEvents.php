c440bc249d8024c79e95be6089b3f4b20e59dfe2
Clyde <klabu123@gmail.com>
2019-12-09 16:00:04 -0500

    created getEvents.php

---
+++ api/getEvents.php
@@ -0,0 +1,23 @@
+<?php
+	include("config.php");
+	include("session.php");
+
+	session_start();
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+
+	$taskname = mysqli_real_escape_string($db,$_POST['taskname']);
+
+	$sql = "select * from events where userid = '{$_SESSION['user_id']}'";
+
+	$result = mysqli_query($db,$sql);
+	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+
+      	echo($row["startdate"]);
+
+
+	}
+
+?>
