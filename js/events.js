8c0cffe7e93c60b5f2681f12e87c7e0101b2adb2
Clyde <klabu123@gmail.com>
2019-12-10 17:48:10 -0500

    update events.js

--- js/events.js
+++ js/events.js
@@ -20,16 +20,19 @@ function getCookie(cname)
   return "";
 }
 
-// card object constructor
-function eventCard(id, eventName, startDate, endDate, pattern, startTime, endTime, building){
+// event object constructor
+function event(id, eventName, startDate, endDate, pattern, startTime, endTime, building){
   this.id = id;
   this.eventName = eventName;
   this.startDate = startDate;
+  this.endDate = endDate;
   this.pattern = pattern;
   this.startTime = startTime;
   this.endTime = endTime;
   this.building = building;
 }
+
+function dayParse()
 
 function getAllEvents(){
   var events[];
@@ -53,7 +56,25 @@ function getAllEvents(){
         var jsonObject = JSON.parse(xhr.responseText);
 
         // error checking?
-        
+        if(jsonObject.length <= 0)
+        {
+          console.log("No events found");
+          return;
+        }
+
+        var numEvents = jsonObject.length;
+
+        for (int i = 0; i < numEvents; i++){
+          var args = [0];
+          args[0] = jsonObject[i].id;
+          args[1] = jsonObject[i].taskname;
+          args[2] = jsonObject[i].startdate;
+          args[3] = jsonObject[i].enddate;
+          args[4] = dayParse(jsonObject);
+          events.push(new event(args[0], args[1], args[2]
+                      , jsonObject[i].enddate,
+                      json))
+        }
       }
     }
   }
