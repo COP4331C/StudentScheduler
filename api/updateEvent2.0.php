457d58ea21c62281f0d80d827675ce59cb18fa90
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-12-10 18:51:07 -0500

    Add files via upload

---
+++ api/updateEvent2.0.php
@@ -0,0 +1,68 @@
+<?php
+	include("config.php");
+	include("session.php");
+	$inData = json_decode(file_get_contents('php://input'), true);
+
+	$taskname = mysqli_real_escape_string($db,$_POST['taskname']);
+	if ($inData['mon'] == '1')
+	{
+	 	$mon = '1';
+	}
+	else
+	{
+	 	$mon = '0';
+	}
+	if ($inData['tue'] == '1')
+	{
+	 	$tue = "1";
+	}
+	else
+	{
+	 	$tue = "0";
+	}
+	if ($inData['wen'] == 1)
+	{
+	 	$wen = '1';
+	}
+	else
+	{
+	 	$wen = '0';
+	}
+	if ($inData['thr'] == 1)
+	{
+	 	$thr = '1';
+	}
+	else
+	{
+	 	$thr = '0';
+	}
+	if ($inData['fri'] == 1)
+	{
+	 	$fri = '1';
+	}
+	else
+	{
+	 	$fri = '0';
+	}
+	if ($inData['sat'] == 1)
+	{
+		 $sat = '1';
+	 }
+	 else
+	 {
+	 	$sat = '0';
+	}
+	if ($inData['sun'] == 1)
+	{
+	 	$sun = '1';
+	}
+	else
+	{
+	 	$sun = '0';
+	}
+
+	$sql = "update events set startdate = '{$_inData['startdate']}', enddate = '{$_inData['enddate']}', starttime = '{$_inData['starttime']}', endtime = '{$inData['endtime']}', mon = '{$mon}', tues = '{$tue}', wen = '{$wen}', thr = '{$thr}', fri = '{$fri}', sat = '{$sat}', sun = '{$sun}', building = '{$inData['building']}' where id = '$inData['id']' ";
+      echo($sql);
+	$result = mysqli_query($db,$sql);
+
+?>
